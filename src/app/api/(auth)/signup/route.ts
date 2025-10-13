de6069fdd19c53f28872dbe291aab9e8a8d61380
Rishabhraj15298 <rishabhraj829112@gmail.com>
2025-10-14 04:01:10 +0530

    added signup functionality

---
+++ src/app/api/(auth)/signup/route.ts
@@ -0,0 +1,25 @@
+import {PrismaClient} from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import {NextResponse} from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function POST(req : Request) {
+    const {username , email , password} = await req.json();
+
+   
+    const existingUser = await prisma.user.findUnique({
+        where : {email}
+
+    })
+    if(existingUser){
+        return NextResponse.json({message : 'User already exists'},{status : 400});
+    }
+    const hashPassword = await bcrypt.hash(password,10);
+
+    const user = await prisma.user.create({
+        data : {username , email , password : hashPassword}
+    
+    })
+    return NextResponse.json({user},{status : 201});
+ }
